5e4b07d9f89ad07ee805204cb696de3421d9f465
CarinhaDaEsquina7243 <rafaeloliveirams7234@gmail.com>
2024-12-29 14:27:10 -0300

    Criando posições randômicas

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,7 @@
 var height = 0;
 var width = 0;
 
-// FUnção para ajustar as dimeesões da janela do browser
+// Função para ajustar as dimeesões da janela do browser
 function adjustWindowSizeData() {
 
 // Acessando o tamanho dos eixos x e y
@@ -10,3 +10,24 @@ width = innerWidth;
 
 console.log(height, width);
 }
+
+adjustWindowSizeData();
+
+// Criando posições randômicas
+var positionY = Math.floor(Math.random() * height - 90);
+var positionX = Math.floor(Math.random() * width - 90);
+
+// Controle de posição para evitar resultado 0
+positionX = positionX < 0 ? 0 : positionX;
+positionY = positionY < 0 ? 0 : positionY
+
+console.log(positionX, positionY);
+
+// Criar o elemento html
+var fly = document.createElement('img');
+fly.src = '../assets/images/mosca.png';
+fly.className = 'fly';
+fly.style.left = positionX + 'px';
+fly.style.top = positionY + 'px';
+fly.style.position = 'absolute'
+document.body.appendChild(fly);
