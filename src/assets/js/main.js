9f14f699aeee73813b468cf0c9e8a9558795a904
CarinhaDaEsquina7243 <rafaeloliveirams7234@gmail.com>
2024-12-29 13:42:12 -0300

    Criando uma dimensão de palco de jogo flexível

---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+var height = 0;
+var width = 0;
+
+// FUnção para ajustar as dimeesões da janela do browser
+function adjustWindowSizeData() {
+
+// Acessando o tamanho dos eixos x e y
+height = window.innerHeight;
+width = innerWidth;
+
+console.log(height, width);
+}
